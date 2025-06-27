952c838f1a40f2f2b783bc910375e9c2495007d0
Antoine Libert <contact@antoine-libert.com>
2025-06-27 18:05:18 +0200

    fix: useGroupsQuery request

--- src/services/jumper/groups.ts
+++ src/services/jumper/groups.ts
@@ -1,15 +1,15 @@
-import type { Group, Page } from '@@types'
+import type { DetailedGroup, Page } from '@@types'
 import { jumperClient, JumperBackendError } from '@/services/jumper/client'
 
-export const getGroups = async (params: {
+export const getDetailedGroups = async (params: {
   page?: number
   limit?: number
   search?: string
   ordering?: string
 }) => {
   const { page = 1, limit = 25, search, ordering } = params
-  const response = await jumperClient.get<Page<Group>>('/v1/groups', {
-    params: { page, limit, search, ordering }
+  const response = await jumperClient.get<Page<DetailedGroup>>('/v1/groups', {
+    params: { page, limit, search, ordering, detailed: true }
   })
   if (response.status !== 200) throw new JumperBackendError(response)
   return response.data
