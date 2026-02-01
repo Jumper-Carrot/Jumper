a2ba3de4d79272905be48f29d447cc1d83ee25f4
Antoine Libert <contact@antoine-libert.com>
2026-02-01 10:31:58 +0100

    refactor(useVersions): consider delay field in versionning

--- src/components/BackOfficePage/ActionsPage/ActionDetailsSection/useVersions.ts
+++ src/components/BackOfficePage/ActionsPage/ActionDetailsSection/useVersions.ts
@@ -1,8 +1,11 @@
-import { ActionVersion, DetailedAction, ActionData } from '@@types/action'
+import type { User } from '@@types'
 import type { ActionDetailedForm } from './useActionDetailedForm'
-import { useQuery } from '@/composables'
-import jumper from '@/services/jumper'
+
 import { computed, MaybeRefOrGetter, ref, toRef } from 'vue'
+import { ActionData, ActionVersion, DetailedAction } from '@@types/action'
+
+import jumper from '@/services/jumper'
+import { useQuery } from '@/composables'
 
 export type VersionsComposable = ReturnType<typeof useVersions>
 
@@ -38,15 +41,23 @@ export const useVersions = (
       isActive: version.isActive,
       thumbnailKey: version.thumbnailKey,
       permissions: [
-        ...version.users,
+        ...(version.users || []).filter((u): u is User => 'externalId' in u),
         ...version.groups,
         ...version.roles
-      ]
+      ],
+      hasDelayBeforeRelaunch: version.hasDelayBeforeRelaunch ?? false,
+      delayBeforeRelaunch: version.delayBeforeRelaunch ?? 1000
     })
   }
 
   const differentDataFields = computed(() => {
-    if (!showVersionsRef.value || !actionRef.value || !selectedVersion.value || !form.values.data) return []
+    if (
+      !showVersionsRef.value ||
+      !actionRef.value ||
+      !selectedVersion.value ||
+      !form.values.data
+    )
+      return []
     const actionData = actionRef.value.data
     const formData = form.values.data
     return Object.entries(actionData).reduce((acc, [key, value]) => {
