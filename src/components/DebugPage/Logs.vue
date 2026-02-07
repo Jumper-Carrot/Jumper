4ccef4e43d988a62f4005a86775dcaea1d794e48
Antoine Libert <contact@antoine-libert.com>
2026-02-07 15:35:43 +0100

    feat: implement DynamicScroller for logs

--- src/components/DebugPage/Logs.vue
+++ src/components/DebugPage/Logs.vue
@@ -1,30 +1,51 @@
 <template>
   <div
     ref="logsContainer"
-    @scroll="handleScroll"
     class="log-scrollbar grow overflow-auto bg-slate-200 shadow-inner
       dark:bg-slate-800"
   >
-    <div
-      v-for="(log, i) in showedLogs"
-      :key="log.timestamp"
-      class="px-2 font-mono whitespace-pre-wrap"
-      :class="{
-        'text-slate-800 dark:text-slate-300':
-          log.level !== 'error' && log.level !== 'warn',
-        'bg-slate-300/50 dark:bg-slate-700/40':
-          i % 2 === 0 && log.level !== 'error' && log.level !== 'warn',
-        [`bg-red-100 font-semibold text-red-500/70 dark:bg-slate-800
-        dark:text-red-400/80`]: log.level === 'error'
-      }"
+    <DynamicScroller
+      :items="showedLogs"
+      :min-item-size="32"
+      key-field="timestamp"
+      :onScroll="handleScroll"
+      class="h-[calc(100vh-54px)] grow"
     >
-      <template v-if="showLineInfo"
-        >{{ log.timestamp }} [{{ log.namespace }}]
-        <span> {{ log.option ? ` | ${log.option}` : '' }} </span> -
-        {{ log.execId.slice(0, 4) }}]:</template
-      >
-      <span v-html="formatAnsi(log.message)"></span>
-    </div>
+      <template v-slot="{ item: log, index: i, active }">
+        <DynamicScrollerItem
+          :item="log"
+          :active="active"
+          :size-dependencies="[
+            log.message,
+            log.level,
+            log.option,
+            log.namespace,
+            log.timestamp,
+            showLineInfo
+          ]"
+          :data-index="i"
+        >
+          <div
+            class="px-2 font-mono whitespace-pre-wrap"
+            :class="{
+              'text-slate-800 dark:text-slate-300':
+                log.level !== 'error' && log.level !== 'warn',
+              'bg-slate-300/50 dark:bg-slate-700/40':
+                i % 2 === 0 && log.level !== 'error' && log.level !== 'warn',
+              [`bg-red-100 font-semibold text-red-500/70 dark:bg-slate-800
+              dark:text-red-400/80`]: log.level === 'error'
+            }"
+          >
+            <template v-if="showLineInfo">
+              {{ log.timestamp }} [{{ log.namespace }}]
+              <span> {{ log.option ? ` | ${log.option}` : '' }} </span> -
+              {{ log.execId.slice(0, 4) }}]:
+            </template>
+            <span v-html="formatAnsi(log.message)"></span>
+          </div>
+        </DynamicScrollerItem>
+      </template>
+    </DynamicScroller>
   </div>
 </template>
 
@@ -33,8 +54,8 @@ import type { Log } from '@/stores/logsStore'
 
 import { ref } from 'vue'
 import { AnsiUp } from 'ansi_up'
-
-// Import de la bibliothèque
+import { DynamicScroller, DynamicScrollerItem } from 'vue-virtual-scroller'
+import 'vue-virtual-scroller/dist/vue-virtual-scroller.css'
 
 import { useScrollToBottom } from './useScrollToBottom'
 
@@ -44,14 +65,11 @@ const props = defineProps<{
 }>()
 
 const ansiUp = new AnsiUp()
-ansiUp.use_classes = false // Utilise des styles inline (plus simple pour débuter)
-
-// Fonction pour transformer l'ANSI en HTML
-const formatAnsi = (message: string) => {
-  return ansiUp.ansi_to_html(message)
-}
+ansiUp.use_classes = false
+const formatAnsi = (message: string) => ansiUp.ansi_to_html(message)
 
 const logsContainer = ref<HTMLDivElement | null>(null)
+
 const { handleScroll } = useScrollToBottom(
   logsContainer,
   () => props.showedLogs.length
